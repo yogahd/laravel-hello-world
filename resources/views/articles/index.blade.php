95ac958ae91b3773c6fb54178f4e8c257e71147c
YogaRahmahadi <yogarahmahadi1234@gmail.com>
2022-04-18 19:57:16 +0700

    Add edit article feature

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -17,7 +17,7 @@
         
         @foreach ($articles as $article)
             <tr>
-                <td>{{ $article->title }}</td>
+                <td><a href="{{ route('articles.edit', ['article' => $article->id]) }}">{{ $article->title }}</a></td>
                 <td>{{ $article->content }}</td>
                 <td><img width="150px"src="{{asset('storage/'.$article->featured_image)}}"></td>
             </tr>
